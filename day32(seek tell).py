0e1fca474de157b979f9856484fc01f74fe37d88
Vishal Kashyap <singhvishalk165@gmail.com>
2024-12-12 14:22:04 +0530

    python

---
+++ day32(seek tell).py
@@ -0,0 +1,14 @@
+# with open('file.txt','w') as f:
+#     print(f)   # use kiye naya text file crate krne ke liye 
+
+    # NOW
+
+with open('file.txt','r') as f:
+    print(type(f))
+    f.seek(11) #move 11 byte in the file 
+    print(f.tell())
+    data =f.read(5) # wha se next 5 character read karega 
+    print(data)
+
+
+ 
---
+++ day33(truncate).py
@@ -0,0 +1,4 @@
+with open ('filetru.txt','w') as f :
+    print(f)
+    f.write('Hello World!')
+    f.truncate(5) # ye 5 byte tk hi truncate kr dega jo v wirte karana hoga so 
