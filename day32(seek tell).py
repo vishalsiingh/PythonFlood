e4500f4f2f764beeb3371e4eea389c0a7486c325
Vishal Kashyap <singhvishalk165@gmail.com>
2024-12-12 14:28:58 +0530

    python

--- day32(seek tell).py
+++ day32(seek tell).py
@@ -6,7 +6,7 @@
 with open('file.txt','r') as f:
     print(type(f))
     f.seek(11) #move 11 byte in the file 
-    print(f.tell())
+    print(f.tell()) # ye current postion return kr dega 
     data =f.read(5) # wha se next 5 character read karega 
     print(data)
 
