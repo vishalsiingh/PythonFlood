d3839dc03fe0df22b5cf2622fea24aeed178c665
Vishal Kashyap <singhvishalk165@gmail.com>
2024-12-16 21:53:15 +0530

    python

---
+++ day38(classes and objects).py
@@ -0,0 +1,10 @@
+class person:
+    name="Vishal"
+    roll=3215
+    cgpa=9.0
+    def info(self):
+        print(f"{self.name} have {self.cgpa}")
+a=person()
+# a.name="Kashyap"
+# print(a.name)
+a.info() # method call kra rhe h 
