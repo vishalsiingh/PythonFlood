a2040f5852674fbb87d9f4eb5a65c5e6f3eaefef
Vishal Kashyap <singhvishalk165@gmail.com>
2024-12-17 23:40:16 +0530

    Text Alignment

---
+++ ex-7(Text alignment).py
@@ -0,0 +1,13 @@
+text = "Python"
+
+
+left_aligned = text.ljust(20)  
+print(f"Left aligned: '{left_aligned}'")
+
+
+right_aligned = text.rjust(20)  
+print(f"Right aligned: '{right_aligned}'")
+
+
+center_aligned = text.center(20)  
+print(f"Center aligned: '{center_aligned}'")
