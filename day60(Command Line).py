d09b80039b5e6df407120b9d238f1186f36728cb
Vishal Kashyap <singhvishalk165@gmail.com>
2024-12-25 23:14:59 +0530

    python

---
+++ day60(Command Line).py
@@ -0,0 +1,44 @@
+# import argparse
+
+# parser = argparse.ArgumentParser()
+
+# parser.add_argument("url",
+# help="url of the file to download"  )
+
+# parser.add_argument("output",
+# help="by which name you want to save"  )  
+
+# args = parser.parse_args()
+# print(args.url)
+# print(args.output)
+import argparse
+import requests
+
+def download_file(url, local_filename): 
+  if local_filename is None:
+    local_filename = url.split('/')[-1]
+    # NOTE the stream=True parameter below
+  with requests.get(url, stream=True) as r:
+      r.raise_for_status()
+      with open(local_filename, 'wb') as f:
+          for chunk in r.iter_content(chunk_size=8192): 
+              # If you have chunk encoded response uncomment if
+              # and set chunk_size parameter to None.
+              #if chunk: 
+              f.write(chunk)
+  return local_filename
+  
+parser = argparse.ArgumentParser()
+
+# Add command line arguments
+parser.add_argument("url", help="Url of the file to download")
+# parser.add_argument("output", help="by which name do you want to save your file")
+parser.add_argument("-o", "--output", type=str, help="Name of the file", default=None)
+
+# Parse the arguments
+args = parser.parse_args()
+
+# Use the arguments in your code
+print(args.url)
+print(args.output, type(args.output))
+download_file(args.url, args.output)
