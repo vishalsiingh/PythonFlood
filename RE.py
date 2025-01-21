bffb20543fdc19b8f71a6eb439aa4eab98f46dbe
Vishal Kashyap <singhvishalk165@gmail.com>
2025-01-21 21:15:00 +0530

    Create RE.py

---
+++ RE.py
@@ -0,0 +1,6 @@
+my_list = [1, 2, 3]
+my_list.append(4)          # Adds an element
+my_list.remove(2)          # Removes an element
+my_list.pop()              # Removes and returns the last element
+my_list.insert(1, 'a')     # Inserts 'a' at index 1
+print(len(my_list))        # Length of the list
